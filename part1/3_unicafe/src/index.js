e38d67bfc4ec0aa81b58ee89e1d1f7dce342f5fd
b30wulffz <shlokpandey123@gmail.com>
2019-07-13 01:26:45 +0530

    Exercise 1.7 Completed

--- part1/3_unicafe/src/index.js
+++ part1/3_unicafe/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-  return (
+    const total = good+neutral+bad;
+    const avg = total ? ((good*1) + (neutral*0) + (bad*(-1)))/total : 0;
+    const pos = total ? (good/total)*100 : 0;
+
+    return (
     <div>
         <h1>give feedback</h1>
         <div>
@@ -22,6 +26,12 @@ const App = () => {
             neutral {neutral}
             <br />
             bad {bad}
+            <br />
+            total {good+neutral+bad}
+            <br />
+            average {avg}
+            <br />
+            positive {pos} %
         </div>
     </div>
   )
