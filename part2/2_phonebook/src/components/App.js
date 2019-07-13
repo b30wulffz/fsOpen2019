7c32587af214ffc5a3194354111a8d328ca2c4bc
b30wulffz <shlokpandey123@gmail.com>
2019-07-13 17:52:39 +0530

    Exercise 2.9 Completed

--- part2/2_phonebook/src/components/App.js
+++ part2/2_phonebook/src/components/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchName, setSearchName ] = useState('')
+  const [ displayPersons, setDisplayPersons ] = useState([...persons])
+  
 
   const updateName = (event) => {
     setNewName(event.target.value);
@@ -26,6 +32,7 @@ const App = () => {
       const copy = [...persons];
       copy.push(data);
       setPersons(copy);
+      setDisplayPersons([...persons])
     }
     else {
       alert(`${newName} is already added to phonebook`);
@@ -33,7 +40,7 @@ const App = () => {
     
   }
 
-  const printData = persons.map((person) => {
+  const printData = displayPersons.map((person) => {
     return (
       <React.Fragment key={person.name}>
         {person.name} {person.number}
@@ -42,9 +49,26 @@ const App = () => {
     )
   });
 
+  const search = (event) => {
+    setSearchName(event.target.value);
+    const data = persons.filter(a=> a.name.toLowerCase().includes( searchName.toLowerCase() ));
+    if(data.length === 0 && searchName.length === 0) {
+      setDisplayPersons([...persons]);
+    }
+    else {
+      setDisplayPersons([...data]);
+    }
+    
+  } 
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input value={searchName} onChange={search} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addData}>
         <div>
           name: <input value={newName} onChange={updateName} />
