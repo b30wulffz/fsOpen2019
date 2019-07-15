82469f6d29dbd7c52076fe7a3c5a7ff9a7b163aa
b30wulffz <shlokpandey123@gmail.com>
2019-07-15 14:50:46 +0530

    Exercise 2.11 Completed

--- part2/2_phonebook/src/components/App.js
+++ part2/2_phonebook/src/components/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return (
@@ -33,17 +34,23 @@ const Persons = ({printData}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  const [persons, setPersons] = useState([])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchName, setSearchName ] = useState('')
   const [ displayPersons, setDisplayPersons ] = useState([...persons])
   
+  
+  useEffect(()=>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response=>{
+        setPersons(response.data);
+        setDisplayPersons(response.data);
+      })
+  },[]);
 
   const updateName = (event) => {
     setNewName(event.target.value);
